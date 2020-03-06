5f856325b96d404ce1eea302d3bed23f5b095a9c
pogchamper314 <60221578+pogchamper314@users.noreply.github.com>
2020-03-06 13:54:34 +0300

    Add files via upload

---
+++ db_php.php
@@ -0,0 +1,35 @@
+<?php
+$settings = [
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => 'lisko314314',
+    'db_name' => 'usersdb'
+];
+
+$link = mysqli_connect($settings['host'], $settings['user'], $settings['password'], $settings['db_name']);
+if(!$link){
+    die('Db connection Error!');
+}
+
+$fopen=file("Log.txt");
+$ip="7";
+$speed="10";
+echo $fopen[$ip];
+echo $fopen[$speed];
+
+$sql = "INSERT INTO arguments (ip, speed) VALUES ('$fopen[$ip]', '$fopen[$speed]')";
+if (mysqli_query($link, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($link);
+}
+mysqli_close($link);
+
+
+
+/*$fd=fopen("newLog.txt", 'w');
+fwrite($fd, $fopen[$ip]);
+fwrite($fd, $fopen[$speed]);
+fclose($fd);
+ */
+?>
